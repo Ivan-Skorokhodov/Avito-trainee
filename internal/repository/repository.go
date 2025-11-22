455e4c61c0c7d056a6f3399eaa407a90038935f1
Ivan-Skorokhodov <dreikonline5554545@gmail.com>
2025-11-22 20:34:03 +0300

    added docker-compose with postgres image

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,38 @@
 package repository
 
+import (
+	"PRmanager/internal/models"
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
 type RepositoryInterface interface {
-	//TODO: реализовать
+	TeamExists(ctx context.Context, teamName string) (bool, error)
+	CreateTeam(ctx context.Context, team *models.Team) error
 }
 
 func NewRepository() RepositoryInterface {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("cannot connect to db: %v", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("cannot connect to db: %v", err)
+	}
 	return nil
 }
